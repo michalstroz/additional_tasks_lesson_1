0dd1a6b40abc4bf00fa51e6a4ba8229fca30680d
Michał <michalstroz@gmail.com>
2017-12-24 17:33:02 +0100

    First Christmass tree additional task

--- christmass_tree.rb
+++ christmass_tree.rb
@@ -1,9 +1,3 @@
-puts "Program generujący świąteczne drzewko."
-puts "Podaj ile pięter ma mieć choinka: "
-height = gets.to_i
-puts "Podaj jak szeroka ma być choinka(Minimalna szerokość to 7): "
-width =  gets.to_i
-
 def christmass_top (x = 7)
   1.step(7, 2) do |i|
     x.times do
@@ -29,6 +23,27 @@ def christmass_tree_flor (x)
     x -= 1
   end
 end
+def christmass_tree_trunk t
+  t.times do
+    print " "
+  end
+  print "*\n"
+end
 
-christmass_top (width)
-christmass_tree_flor (width)
+puts "Program generujący świąteczne drzewko."
+loop do
+  puts "Podaj ile pięter ma mieć choinka: "
+  height = gets.to_i
+  puts "Podaj jak szeroka ma być choinka(Minimalna szerokość to 7): "
+  width =  gets.to_i
+  if height < 1 && width < 7
+    puts "podano złe parametry"
+  else
+    christmass_top (width)
+    height.times do
+      christmass_tree_flor (width)
+    end
+    christmass_tree_trunk width
+    break
+  end
+end
